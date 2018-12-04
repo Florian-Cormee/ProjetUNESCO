b11d5e224045520d7932ec95da39808617d7ca06
Florian-Cormee <florian.cormee@gmail.com>
2018-12-04 11:49:27 +0100

    utils commente

--- src/site.c
+++ src/site.c
@@ -6,7 +6,7 @@
 #include "site.h"
 
 Site *Site_construire(int n, char *nom, double lat, double lon, char *categorie, char *pays, int enDanger) {
-    Site *site = (Site *) malloc(sizeof(Site));
+    Site *site = (Site *) malloc(sizeof(Site));// site a retourner
     site->n = n;
     site->nom = nom == NULL ? NULL : strdup(nom);
     site->lat = lat;
@@ -24,9 +24,9 @@ void Site_affichage(Site *site) {
 }
 
 int Site_equals(Site *site1, Site *site2) {
-    if (site1 == NULL || site2 == NULL) { return ERROR; }
     if (site1 == site2) { return TRUE; }//Meme adresse memoire
 
+    if (site1 == NULL || site2 == NULL) { return ERROR; }
     if (site1->n != site2->n) { return FALSE; }
     if (site1->lat != site2->lat) { return FALSE; }
     if (site1->lon != site2->lon) { return FALSE; }
@@ -43,59 +43,60 @@ void Site_supprime(Site *site) {
 
 Site **Site_tab_init(int *n) {
     // Mesure du nombre de sites dans le fichier : unesco.csv
-    FILE *fid = fopen("unesco.csv", "r");
-    *n = 0;
+    FILE *fid = fopen("unesco.csv", "r");// Fichier a lire.
+    *n = 0; // initialisation du compteur pour la taille de la liste.
     if (fid != NULL) {// si le fichier existe
         while (!feof(fid)) {// tant qu'on n'a pas atteint la fin du fichier
-            (*n)++;
+            (*n)++; // il y a un nouvel element a lire
             SkipLine(fid);
         }
-        fclose(fid);
+        fclose(fid);// fermeture du fichier
     }
     (*n) -= 2;// On ne compte pas la 1ere ligne ni la derniere qui est vide
     // Chargement des sites
-    fid = fopen("unesco.csv", "r");
+    fid = fopen("unesco.csv", "r");// reouverture du fichier (pour reprendre au debut)
     Site **site = (Site **) malloc(sizeof(Site) * (*n));// tableau de tous les sites
-    char nom[512], categorie[32], pays[256], continents[512];
-    double LAT, LONG;
-    int EnDanger;
+    char nom[512], categorie[32], pays[256], continents[512];// tempons pour la lecture des differents champs
+    double LAT, LONG;// coordonnees du site
+    int EnDanger;// statut du site
 
     if (fid != NULL) {// si le fichier existe
-        SkipLine(fid);
+        SkipLine(fid);// on ignore la premiere ligne (ne decrit pas un site)
         for (int i = 0; i < *n; i++) {
-            GetChaine(fid, 512, nom);
-            LAT = GetReel(fid);
-            LONG = GetReel(fid);
-            GetChaine(fid, 32, categorie);
-            GetChaine(fid, 256, pays);
-            GetChaine(fid, 512, continents);
-            EnDanger = GetEntier(fid);
-            site[i] = Site_construire(i, nom, LAT, LONG, categorie, pays, EnDanger);
+            GetChaine(fid, 512, nom);// lecture du nom
+            LAT = GetReel(fid);// lecture de la latitude
+            LONG = GetReel(fid);// lecture de la longitude
+            GetChaine(fid, 32, categorie); // lecture de la categorie
+            GetChaine(fid, 256, pays);// lecture du pays
+            GetChaine(fid, 512, continents);// lecture du continent
+            EnDanger = GetEntier(fid);// lecture du statut
+            site[i] = Site_construire(i, nom, LAT, LONG, categorie, pays, EnDanger);// creation du site
         }
-        fclose(fid);
+        fclose(fid);// fermeture du fichier
     }
     return site;
 }
 
 void Site_tab_supprime(Site **pSite, int n) {
     if (pSite == NULL) { return; }
-    for (int i = 0; i < n; i++) {
-        free(*(pSite + i));
+    for (int i = 0; i < n; i++) {// iteration sur tous les elements
+        Site_supprime(pSite[i]); // liberation du site
     }
+    free(pSite);//liberation du tableau
 }
 
 /*  ---------------- LDC ---------------- */
 LDC *LDC_nouveau() {
-    LDC *ldc = (LDC *) malloc(sizeof(LDC));
-    ldc->premier = NULL;
-    ldc->dernier = NULL;
+    LDC *ldc = (LDC *) malloc(sizeof(LDC));//nouvelle liste
+    ldc->premier = NULL;//valeur par defaut
+    ldc->dernier = NULL;//valeur par defaut
 }
 
 int LDC_taille(LDC *list) {
     if (list == NULL) { return ERROR; }
     int size = 0; // compteur d'elements de la liste
 
-    for (CelluleLDC *cell = list->premier; cell != NULL; cell = cell->suiv) {
+    for (CelluleLDC *cell = list->premier; cell != NULL; cell = cell->suiv) {//iteration sur les cellules de la liste
         size++;
     }
 
@@ -105,7 +106,7 @@ int LDC_taille(LDC *list) {
 void LDC_affiche(LDC *ldc) {
     printf("LDC {\n");
 
-    for (CelluleLDC *cell = ldc->premier; cell != NULL; cell = cell->suiv) {
+    for (CelluleLDC *cell = ldc->premier; cell != NULL; cell = cell->suiv) {//iteration sur les cellules de la liste
         printf("\t%p > prec = %p, suiv= %p, s=", cell, cell->prec, cell->suiv);
         Site_affichage(cell->s);
     }
@@ -115,10 +116,10 @@ void LDC_affiche(LDC *ldc) {
 void LDC_ajoute_deb(LDC *ldc, Site *site) {
     if (ldc == NULL || site == NULL) { return; }
 
-    CelluleLDC *cell = (CelluleLDC *) malloc(sizeof(CelluleLDC));
-    cell->s = site;
-    cell->prec = NULL;
-    cell->suiv = NULL;
+    CelluleLDC *cell = (CelluleLDC *) malloc(sizeof(CelluleLDC));// nouvelle cellule
+    cell->s = site;// valeur de la cellule
+    cell->prec = NULL;// valeur par defaut
+    cell->suiv = NULL;// valeur par defaut
 
     if (ldc->dernier == NULL) {// si la liste est vide
         ldc->premier = cell;
@@ -134,10 +135,10 @@ void LDC_ajoute_deb(LDC *ldc, Site *site) {
 void LDC_ajoute_fin(LDC *ldc, Site *site) {
     if (ldc == NULL || site == NULL) { return; }
 
-    CelluleLDC *cell = (CelluleLDC *) malloc(sizeof(CelluleLDC));
-    cell->s = site;
-    cell->prec = NULL;
-    cell->suiv = NULL;
+    CelluleLDC *cell = (CelluleLDC *) malloc(sizeof(CelluleLDC));// nouvelle cellule
+    cell->s = site;// valeur de la cellule
+    cell->prec = NULL;// valeur par defaut
+    cell->suiv = NULL;// valeur par defaut
 
     if (ldc->dernier == NULL) {// si la liste est vide
         ldc->premier = cell;
@@ -153,69 +154,65 @@ void LDC_ajoute_fin(LDC *ldc, Site *site) {
 int LDC_trouve(LDC *ldc, Site *site) {
     if (ldc == NULL || site == NULL) { return ERROR; }
 
-    int i = 0;
+    int i = 0;// indice de la cellule consultee
 
-    for (CelluleLDC *cell = ldc->premier; cell != NULL; cell = cell->suiv) {
-        if (Site_equals(cell->s, site)) {
+    for (CelluleLDC *cell = ldc->premier; cell != NULL; cell = cell->suiv) {// iteration sur la liste
+        if (Site_equals(cell->s, site)) {// s'il y a equivalence entre la valeur de la cellule et site
             return i;
         }
         i++;
     }
-    return ERROR;
+    return ERROR;//l'element est absent de la liste
 }
 
 Site *LDC_get(LDC *ldc, int i) {
     if (ldc == NULL || i < 0) { return NULL; }
 
-    CelluleLDC *courant = ldc->premier;
+    CelluleLDC *courant = ldc->premier;// cellule courante
 
-    for (int index = 0; index < i && courant != NULL; index++) {
+    for (int index = 0; index < i && courant != NULL; index++) {//iteration jusqu'a index = i ou fin de liste
         printf("ldc.get(%3d) = %p\n", index, courant->s);
         courant = courant->suiv;
     }
 
-    if (courant == NULL) { return NULL; }
+    if (courant == NULL) { return NULL; }//fin de la liste atteinte
     return courant->s;
 }
 
 void LDC_rm(LDC *ldc, Site *site) {
     if (ldc == NULL || site == NULL) { return; }
 
-    CelluleLDC *courant = ldc->premier;
+    CelluleLDC *courant = ldc->premier;// cellule a supprimer
 
-    while (courant != NULL && !Site_equals(courant->s, site)) {
+    while (courant != NULL && !Site_equals(courant->s, site)) {// recherche de la cellule a supprimer
         courant = courant->suiv;
     }
 
     if (courant == NULL) { return; }
 
     if (courant->prec == NULL && courant->suiv == NULL) {//c'est l'unique element de la liste
-        // printf("rm unique element\n");
         ldc->premier = NULL;
         ldc->dernier = NULL;
     } else if (courant->prec == NULL) {//c'est le premier element de la liste
-        // printf("rm deb\n");
         (courant->suiv)->prec = NULL;
         ldc->premier = courant->suiv;
     } else if (courant->suiv == NULL) {//c'est le dernier element de la liste
-        // printf("rm fin\n");
         (courant->prec)->suiv = NULL;
         ldc->dernier = courant->prec;
     } else {
-        // printf("rm autre\n");
         (courant->prec)->suiv = courant->suiv;
         (courant->suiv)->prec = courant->prec;
     }
-    free(courant);
+    free(courant);// liberation de la cellule
 }
 
 void LDC_free(LDC **ldc, int freeSite) {
     if (ldc == NULL || *ldc == NULL) { return; }
 
-    for (CelluleLDC *cell = (*ldc)->premier; cell != NULL; cell = (*ldc)->premier) {
+    for (CelluleLDC *cell = (*ldc)->premier; cell != NULL; cell = (*ldc)->premier) {//iteration sur la liste
         (*ldc)->premier = cell->suiv;
-        if (freeSite) {
-            free(cell->s);
+        if (freeSite) {// si on doit liberer les sites
+            Site_supprime(cell->s);
         }
         free(cell);
     }
@@ -227,5 +224,5 @@ int LDC_empty(LDC *ldc) {
     if (ldc == NULL) {
         return 1;
     }
-    return ldc->premier == NULL ? 1 : 0;
+    return ldc->premier == NULL ? 1 : 0;// la liste est consideree vide si le pointeur vers le premier element est NULL
 }
--- src/site.h
+++ src/site.h
@@ -10,14 +10,14 @@ typedef struct site {
     double lon;
     char *categorie; //cultural,natural,mixed
     char *pays;
-    int enDanger; //0,1
+    int enDanger; //vaut TRUE ou FALSE
 } Site;
 
 typedef struct celluleLDC {
     Site *s;
     struct celluleLDC *prec;
     struct celluleLDC *suiv;
-} CelluleLDC;//Elem;
+} CelluleLDC;
 
 typedef struct ldc {
     CelluleLDC *premier;
@@ -28,38 +28,124 @@ typedef struct ldc {
 // -------------------------------------- //
 //                  SITE                  //
 // -------------------------------------- //
+
+/**
+ * Cree un site en initialisant ses variables.
+ * @param n Identifiant du site (pour la matrice des distances notamment.
+ * @param nom Nom du site.
+ * @param homeLat Latitude du point de depart.
+ * @param homeLon Longitude du point de depart.
+ * @param categorie Categorie du site.
+ * @param pays Pays ou se trouve le site.
+ * @param enDanger (booleen) TRUE si en danger sinon FALSE.
+ * @return Pointeur vers le site cree.
+ */
 Site *Site_construire(int n, char *nom, double lat, double lon, char *categorie, char *pays, int enDanger);
 
+/**
+ * Affiche un site dans la console
+ * @param site Pointeur vers le site a afficher
+ */
 void Site_affichage(Site *site);
 
+/**
+ * Test d'equivalence entre deux sites.
+ * @param site1 Pointeur vers un site.
+ * @param site2 Pointeur vers un site.
+ * @return TRUE si champ à champ les sites sont egaux, sinon FALSE.
+ */
 int Site_equals(Site *site1, Site *site2);
 
+/**
+ * Libere la memoire allouee pour un site
+ * @param site Pointeur vers le site a liberer
+ */
 void Site_supprime(Site *site);
 
+/**
+ * Charge les sites depuis "unesco.csv".
+ * @param Pointeur vers la taille du tableau.
+ * @return Le tableau des sites charges.
+ */
 Site **Site_tab_init(int *n);
 
+/**
+ * Libere la memoire allouee au tableau des sites.
+ * @param pSite Tableau des sites.
+ * @param n Taille du tableau.
+ */
 void Site_tab_supprime(Site **pSite, int n);
 
 // -------------------------------------- //
 //                  LDC                   //
 // -------------------------------------- //
+/**
+ * Cree une nouvelle liste doublement chainee.
+ * @return Un pointeur vers la liste nouvellement cree.
+ */
 LDC *LDC_nouveau();
 
+/**
+ * Donne la taille de la liste.
+ * @param ldc Liste de sites.
+ * @return La taille de la liste.
+ */
 int LDC_taille(LDC *ldc);
 
+/**
+ * Affiche la liste (dans le detail) dans la console
+ * @param ldc Liste a afficher.
+ */
 void LDC_affiche(LDC *ldc);
 
+/**
+ * Ajoute un site au debut d'une liste.
+ * @param ldc Liste a laquelle on ajoute un site.
+ * @param site Site a ajouter.
+ */
 void LDC_ajoute_deb(LDC *ldc, Site *site);
 
+/**
+ * Ajoute un site a la fin d'une liste.
+ * @param ldc Liste a laquelle on ajoute un site.
+ * @param site Site a ajouter.
+ */
 void LDC_ajoute_fin(LDC *ldc, Site *site);
 
+/**
+ * Donne l'indice (dans la liste) correspondant au site.
+ * @param ldc Liste de sites.
+ * @param site Site a chercher.
+ * @return Indice du site dans la liste.
+ */
 int LDC_trouve(LDC *ldc, Site *site);
 
+/**
+ * Donne le site a l'indice donnee dans la liste.
+ * @param ldc Liste de sites.
+ * @param index Indice de l'element a recuperer.
+ * @return Le site a l'indice dans la liste.
+ */
 Site *LDC_get(LDC *ldc, int index);
 
+/**
+ * Retire un site de la liste.
+ * @param ldc Liste de sites.
+ * @param site Site a retirer de la liste.
+ */
 void LDC_rm(LDC *ldc, Site *site);
 
+/**
+ * Libere la memoire alloue a une liste.
+ * @param ldc Pointeur du pointeur vers la liste.
+ * @param freeSite (booleen) TRUE: les sites sont egalements liberes, sinon seuls les cellules le sont.
+ */
 void LDC_free(LDC **ldc, int freeSite);
 
+/**
+ * Indique si la liste est vide.
+ * @param ldc Liste a tester.
+ * @return TRUE si la liste est vide, FALSE sinon.
+ */
 int LDC_empty(LDC *ldc);
 #endif
